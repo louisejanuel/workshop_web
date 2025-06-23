c6cbb404d11e519fed6e422c3b1a2773e5fde082
honorine <honorineru@gmail.com>
2025-06-23 11:51:37 +0200

    initialisation project

---
+++ server.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import random
+import mysql.connector
+from flask import Flask,request,render_template, jsonify
+from flask_cors import CORS
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="archilog"
+)
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def form():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
