208f66d27e6e91af00e45750028f2260baf75e0d
honorine <honorineru@gmail.com>
2025-06-24 14:22:47 +0200

    liked songs profile

--- server.py
+++ server.py
@@ -36,7 +36,7 @@ def register():
         password = request.form['password']
         cursor.execute("INSERT INTO USER (firstName, lastName, userName, password) VALUES (%s, %s, %s, SHA1(%s))", (firstName, lastName, userName, password))
         mydb.commit()
-        return render_template('youp.html')
+        return render_template('profile.html')
     return render_template('register.html')
 
 
@@ -49,15 +49,32 @@ def login():
         user = cursor.fetchone()
         if user:
             session['user'] = userName
-            return render_template('youp.html')
+            session['idUser'] = user[0]
+            return redirect(url_for('profile'))
     return render_template('login.html')
+  
+
+@app.route('/profile', methods=['GET'])
+def profile():
+    user_id = session.get('idUser')
+    if not user_id:
+        return redirect(url_for('login'))
+    cursor.execute("""
+        SELECT MUSIC.title 
+        FROM LIKED 
+        JOIN MUSIC ON LIKED.idMusic = MUSIC.idMusic 
+        WHERE LIKED.idUser = %s
+    """, (user_id,))
+    results = cursor.fetchall()
+    print(results)
+    return render_template('profile.html', data=results)
+
 
 
 @app.route('/logout')
 def logout():
     session.pop('user', None)
     return redirect(url_for('home'))
-
 
 
 if __name__ == "__main__":
