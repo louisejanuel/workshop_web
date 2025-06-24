78b6dfe3604157dfb3874550e4c421f84600199d
lou'ise <louloujanu@gmail.com>
2025-06-24 16:38:31 +0200

    fix match

--- py/match.py
+++ py/match.py
@@ -9,14 +9,17 @@ def match():
     if not user_id:
         return redirect(url_for('login'))
     cursor.execute("""
-    SELECT DISTINCT 
-        MUSIC.title, 
-        USER.userName 
-        FROM LIKED 
-        JOIN MUSIC ON LIKED.idMusic = MUSIC.idMusic 
-        JOIN USER ON LIKED.idUser = USER.idUser
-        WHERE USER.idUser != %s
-    """, (user_id,))
+    SELECT DISTINCT
+    MUSIC.title,
+    USER.userName
+    FROM LIKED
+    JOIN MUSIC ON LIKED.idMusic = MUSIC.idMusic
+    JOIN USER ON LIKED.idUser = USER.idUser
+    WHERE LIKED.idMusic IN (
+        SELECT idMusic FROM LIKED WHERE idUser = %s
+    )
+    AND USER.idUser != %s;
+    """, (user_id, user_id))
     results = cursor.fetchall()
     print(results)
     return render_template('match.html', data=results)
