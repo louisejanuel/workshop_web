c6fca86e30f1b93c24f3c18f0e4e7d475eb2910c
lou'ise <louloujanu@gmail.com>
2025-06-25 12:02:21 +0200

    fix match2 + css

--- py/match.py
+++ py/match.py
@@ -8,18 +8,24 @@ def match():
     user_id = session.get('idUser')
     if not user_id:
         return redirect(url_for('login'))
+
     cursor.execute("""
-    SELECT DISTINCT
-    MUSIC.title,
-    USER.userName
-    FROM LIKED
-    JOIN MUSIC ON LIKED.idMusic = MUSIC.idMusic
-    JOIN USER ON LIKED.idUser = USER.idUser
-    WHERE LIKED.idMusic IN (
-        SELECT idMusic FROM LIKED WHERE idUser = %s
-    )
-    AND USER.idUser != %s;
+        SELECT 
+            MUSIC.title,
+            GROUP_CONCAT(DISTINCT ARTIST.artistName SEPARATOR ', ') AS artists,
+            USER.userName
+        FROM LIKED
+        JOIN USER ON USER.idUser = LIKED.idUser
+        JOIN MUSIC ON LIKED.idMusic = MUSIC.idMusic
+        JOIN COMPOSED ON MUSIC.idMusic = COMPOSED.idMusic
+        JOIN ARTIST ON COMPOSED.idArtist = ARTIST.idArtist
+        WHERE LIKED.idMusic IN (
+            SELECT idMusic FROM LIKED WHERE idUser = %s
+        )
+        AND USER.idUser != %s
+        GROUP BY MUSIC.title, USER.userName
     """, (user_id, user_id))
+
     results = cursor.fetchall()
-    print(results)
     return render_template('match.html', data=results)
+
