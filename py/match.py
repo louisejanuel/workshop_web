81f712a8f1bd77db21b60fb49d3a2bfbafa517da
lou'ise <louloujanu@gmail.com>
2025-06-25 12:23:26 +0200

    genre

--- py/match.py
+++ py/match.py
@@ -10,22 +10,37 @@ def match():
         return redirect(url_for('login'))
 
     cursor.execute("""
-        SELECT 
-            MUSIC.title,
-            GROUP_CONCAT(DISTINCT ARTIST.artistName SEPARATOR ', ') AS artists,
-            USER.userName
-        FROM LIKED
-        JOIN USER ON USER.idUser = LIKED.idUser
-        JOIN MUSIC ON LIKED.idMusic = MUSIC.idMusic
-        JOIN COMPOSED ON MUSIC.idMusic = COMPOSED.idMusic
-        JOIN ARTIST ON COMPOSED.idArtist = ARTIST.idArtist
-        WHERE LIKED.idMusic IN (
-            SELECT idMusic FROM LIKED WHERE idUser = %s
+        SELECT DISTINCT
+            U.userName,
+            M.title,
+            GROUP_CONCAT(DISTINCT A.artistName SEPARATOR ', ') AS artists,
+            G.genreName
+        FROM LIKED L
+        JOIN USER U ON U.idUser = L.idUser
+        JOIN MUSIC M ON L.idMusic = M.idMusic
+        JOIN GENRE G ON M.idGenre = G.idGenre
+        JOIN COMPOSED C ON M.idMusic = C.idMusic
+        JOIN ARTIST A ON C.idArtist = A.idArtist
+        WHERE (
+            L.idMusic IN (
+                SELECT idMusic FROM LIKED WHERE idUser = %s
+            )
+            OR C.idArtist IN (
+                SELECT C2.idArtist
+                FROM LIKED L2
+                JOIN COMPOSED C2 ON L2.idMusic = C2.idMusic
+                WHERE L2.idUser = %s
+            )
+            OR M.idGenre IN (
+                SELECT M2.idGenre
+                FROM LIKED L2
+                JOIN MUSIC M2 ON L2.idMusic = M2.idMusic
+                WHERE L2.idUser = %s
+            )
         )
-        AND USER.idUser != %s
-        GROUP BY MUSIC.title, USER.userName
-    """, (user_id, user_id))
+        AND U.idUser != %s
+        GROUP BY U.userName, M.title, G.genreName
+    """, (user_id, user_id, user_id, user_id))
 
     results = cursor.fetchall()
     return render_template('match.html', data=results)
-
