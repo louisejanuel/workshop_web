ed7b7607045a333ff6f848988510a5f5cd536d07
honorine <honorineru@gmail.com>
2025-06-26 16:31:47 +0200

    fix match sort

--- models/match_model.py
+++ models/match_model.py
@@ -20,11 +20,12 @@ def get_matches_by_title(user_id):
 def get_matches_by_artist(user_id):
     cursor = mydb.cursor()
     query = """
-        SELECT U.userName, GROUP_CONCAT(DISTINCT M.title SEPARATOR ', ') AS titles
+        SELECT U.userName, GROUP_CONCAT(DISTINCT A.artistName SEPARATOR ', ') AS artists
         FROM LIKED L
         JOIN USER U ON U.idUser = L.idUser
         JOIN MUSIC M ON L.idMusic = M.idMusic
         JOIN COMPOSED C ON M.idMusic = C.idMusic
+        JOIN ARTIST A ON C.idArtist = A.idArtist
         WHERE C.idArtist IN (
             SELECT C2.idArtist
             FROM LIKED L2
@@ -33,11 +34,10 @@ def get_matches_by_artist(user_id):
         )
         AND U.idUser != %s
         GROUP BY U.userName
-        ORDER BY titles
+        ORDER BY artists
     """
     cursor.execute(query, (user_id, user_id))
     return cursor.fetchall()
-
 
 
 def get_matches_by_genre(user_id):
--- models/profile_model.py
+++ models/profile_model.py
@@ -3,13 +3,14 @@ from app_init import mydb
 def user_likes_user(user_id):
     cursor = mydb.cursor()
     cursor.execute("""
-        SELECT MUSIC.title, ARTIST.artistName, GENRE.genreName
+        SELECT MUSIC.title, GROUP_CONCAT(DISTINCT ARTIST.artistName SEPARATOR ', ') AS artists, GENRE.genreName
         FROM LIKED
         JOIN MUSIC ON LIKED.idMusic = MUSIC.idMusic
         JOIN COMPOSED ON MUSIC.idMusic = COMPOSED.idMusic
         JOIN ARTIST ON COMPOSED.idArtist = ARTIST.idArtist
         JOIN GENRE ON MUSIC.idGenre = GENRE.idGenre
         WHERE LIKED.idUser = %s
+        GROUP BY MUSIC.title, GENRE.genreName
     """, (user_id,))
     return cursor.fetchall()
 
